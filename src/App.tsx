aa735402e62f84771595e0a673450a472913f448
Daniel Cuevas Botero <danycubo2002@hotmail.com>
2024-10-16 12:16:04 -0500

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <header className="bg-lime-600 py-3">
+      <header className="bg-lime-600 py-3 items-center">
         <div className="max-w-4xl mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorias</h1>
           <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,4 +1,3 @@
-import type { Category } from "../types"
 
 export const categories = [
     { id: 1, name: 'Comida'},
